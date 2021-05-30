41fc9656ea2679b1bb8c1a0928145f74a88658fe
Christian <christian.ott@mailbox.org>
2021-05-30 19:38:45 +0200

    API Call reduced to one Request

--- io.openems.edge.summary/src/io/openems/edge/summary/SummaryImpl.java
+++ io.openems.edge.summary/src/io/openems/edge/summary/SummaryImpl.java
@@ -7,6 +7,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
 
 import org.osgi.service.component.ComponentContext;
 import org.osgi.service.component.annotations.Activate;
@@ -17,8 +18,8 @@ import org.osgi.service.event.Event;
 import org.osgi.service.event.EventConstants;
 import org.osgi.service.event.EventHandler;
 import org.osgi.service.metatype.annotations.Designate;
-
-import com.google.gson.JsonObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import io.openems.common.exceptions.OpenemsException;
 import io.openems.common.exceptions.OpenemsError.OpenemsNamedException;
@@ -38,6 +39,7 @@ import io.openems.edge.common.event.EdgeEventConstants;
 public class SummaryImpl extends AbstractOpenemsComponent implements Summary, OpenemsComponent, EventHandler {
 
 	private Config config = null;
+	private final Logger log = LoggerFactory.getLogger(SummaryImpl.class);
 	int cycle = 0;
 	int executeEveryCycle = 0;
 	String url = "";
@@ -79,131 +81,171 @@ public class SummaryImpl extends AbstractOpenemsComponent implements Summary, Op
 			// Execute every x-Cycle
 			if (this.cycle == 0 || this.cycle % this.executeEveryCycle == 0) {
 				DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-				String dailyBody = "from(bucket: \"openems/autogen\")\n"
-						  + "|> range(start: " + LocalDateTime.now().format(formatter) + ")\n"  
-						  + "|> filter(fn: (r) => r._measurement == \"data\" and r._field == \"_sum/ProductionActivePower\")\n"
-						  + "|> integral(unit: 1h, column: \"_value\")";	 
-
+				String Today = LocalDateTime.now().format(formatter);
 				String StartMonth = LocalDateTime.now().getYear() + "-" + String.format("%02d", LocalDateTime.now().getMonthValue()) + "-01"; 
-				String weeklyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartMonth + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyProduction\" and r._field == \"integral\")\n"
-						+ "  |> sum()";			
-				
 				String StartYear = LocalDateTime.now().getYear() + "-01-01"; 
-				String yearlyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartYear + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyProduction\" and r._field == \"integral\")\n"
-						+ "  |> sum()";	
+				String SumYear = "2000-01-01";
 				
-				String sumBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: 2000-01-01)\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyProduction\" and r._field == \"integral\")\n"
-						+ "  |> sum()";	
-				
-				JsonObject dailyProd = this.sendPostRequest(this.url, dailyBody);
-				JsonObject monthlyProd = this.sendPostRequest(this.url, weeklyBody);
-				JsonObject yearlyProd = this.sendPostRequest(this.url, yearlyBody);
-				JsonObject sumProd = this.sendPostRequest(this.url, sumBody);
-				
-				int dailyProduction = dailyProd.getAsJsonObject().get("_value").getAsInt();
-				this.channel(Summary.ChannelId.DAILY_PRODUCTION).setNextValue(dailyProduction);
-				this.channel(Summary.ChannelId.MONTHLY_PRODUCTION).setNextValue(monthlyProd.getAsJsonObject().get("_value").getAsInt() + dailyProduction);
-				this.channel(Summary.ChannelId.YEARLY_PRODUCTION).setNextValue(yearlyProd.getAsJsonObject().get("_value").getAsInt() + dailyProduction);
-				this.channel(Summary.ChannelId.SUM_PRODUCTION).setNextValue(sumProd.getAsJsonObject().get("_value").getAsInt() + dailyProduction);
-				
-				dailyBody = "from(bucket: \"openems/autogen\")\n"
-						  + "|> range(start: " + LocalDateTime.now().format(formatter) + ")\n"  
+				String Body = "t1 = from(bucket: \"openems/autogen\")\n"
+						  + "|> range(start: " + Today + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"data\" and r._field == \"_sum/ProductionActivePower\")\n"
+						  + "|> integral(unit: 1h, column: \"_value\")\n"
+						  + "t2 = from(bucket: \"openems/autogen\")\n"
+						  + "|> range(start: " + Today + ")\n"   
 						  + "|> filter(fn: (r) => r._measurement == \"data\" and r._field == \"_sum/ConsumptionActivePower\")\n"
-						  + "|> integral(unit: 1h, column: \"_value\")";	 
-
-				weeklyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartMonth + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyConsumption\" and r._field == \"integral\")\n"
-						+ "  |> sum()";			
-				
-				yearlyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartYear + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyConsumption\" and r._field == \"integral\")\n"
-						+ "  |> sum()";	
-				
-				sumBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: 2000-01-01)\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyConsumption\" and r._field == \"integral\")\n"
-						+ "  |> sum()";	
-				
-				JsonObject dailyCons = this.sendPostRequest(this.url, dailyBody);
-				JsonObject monthlyCons = this.sendPostRequest(this.url, weeklyBody);
-				JsonObject yearlyCons = this.sendPostRequest(this.url, yearlyBody);
-				JsonObject sumCons = this.sendPostRequest(this.url, sumBody);
-				
-				int dailyConsumption = dailyCons.getAsJsonObject().get("_value").getAsInt();
-				this.channel(Summary.ChannelId.DAILY_CONSUMPTION).setNextValue(dailyConsumption);
-				this.channel(Summary.ChannelId.MONTHLY_CONSUMPTION).setNextValue(monthlyCons.getAsJsonObject().get("_value").getAsInt() + dailyConsumption);
-				this.channel(Summary.ChannelId.YEARLY_CONSUMPTION).setNextValue(yearlyCons.getAsJsonObject().get("_value").getAsInt() + dailyConsumption);
-				this.channel(Summary.ChannelId.SUM_CONSUMPTION).setNextValue(sumCons.getAsJsonObject().get("_value").getAsInt() + dailyConsumption);
-
-				dailyBody = "from(bucket: \"openems/autogen\")\n"
-						  + "|> range(start: " + LocalDateTime.now().format(formatter) + ")\n"  
+						  + "|> integral(unit: 1h, column: \"_value\")\n"
+						  + "t3 = from(bucket: \"openems/autogen\")\n"
+						  + "|> range(start: " + Today + ")\n"   
 						  + "|> filter(fn: (r) => r._measurement == \"data\" and r._field == \"_sum/GridActivePower\" and r._value < 0)\n"
-						  + "|> integral(unit: 1h, column: \"_value\")";	 
-
-				weeklyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartMonth + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailySell\" and r._field == \"abs\")\n"
-						+ "  |> sum()";			
-				
-				yearlyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartYear + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailySell\" and r._field == \"abs\")\n"
-						+ "  |> sum()";	
-				
-				sumBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: 2000-01-01)\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailySell\" and r._field == \"abs\")\n"
-						+ "  |> sum()";	
-				
-				JsonObject dailySell = this.sendPostRequest(this.url, dailyBody);
-				JsonObject monthlySell = this.sendPostRequest(this.url, weeklyBody);
-				JsonObject yearlySell = this.sendPostRequest(this.url, yearlyBody);
-				JsonObject sumSell = this.sendPostRequest(this.url, sumBody);
-				
-				int idailySell = Math.abs(dailySell.getAsJsonObject().get("_value").getAsInt());
-				this.channel(Summary.ChannelId.DAILY_SELL).setNextValue(idailySell);
-				this.channel(Summary.ChannelId.MONTHLY_SELL).setNextValue(Math.abs(monthlySell.getAsJsonObject().get("_value").getAsInt()) + idailySell);
-				this.channel(Summary.ChannelId.YEARLY_SELL).setNextValue(Math.abs(yearlySell.getAsJsonObject().get("_value").getAsInt() + idailySell));
-				this.channel(Summary.ChannelId.SUM_SELL).setNextValue(Math.abs(sumSell.getAsJsonObject().get("_value").getAsInt() + idailySell));
-				
-				dailyBody = "from(bucket: \"openems/autogen\")\n"
-						  + "|> range(start: " + LocalDateTime.now().format(formatter) + ")\n"  
+						  + "|> integral(unit: 1h, column: \"_value\")\n"
+						  + "t4 = from(bucket: \"openems/autogen\")\n"
+						  + "|> range(start: " + Today + ")\n"  
 						  + "|> filter(fn: (r) => r._measurement == \"data\" and r._field == \"_sum/GridActivePower\" and r._value > 0)\n"
-						  + "|> integral(unit: 1h, column: \"_value\")";	 
-
-				weeklyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartMonth + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyBuy\" and r._field == \"abs\")\n"
-						+ "  |> sum()";			
+						  + "|> integral(unit: 1h, column: \"_value\")\n"
+						  + "t5 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartMonth + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"dailyProduction\")\n"
+						  + "|> sum()\n"
+						  + "t6 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartMonth + ")\n"   
+						  + "|> filter(fn: (r) => r._measurement == \"dailyConsumption\")\n"
+						  + "|> sum()\n"
+						  + "t7 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartMonth + ")\n"   
+						  + "|> filter(fn: (r) => r._measurement == \"dailySell\")\n"
+						  + "|> sum()\n"
+						  + "t8 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartMonth + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"dailyBuy\")\n"
+						  + "|> sum()\n"
+						  + "t9 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartYear + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"dailyProduction\")\n"
+						  + "|> sum()\n"
+						  + "t10 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartYear + ")\n"   
+						  + "|> filter(fn: (r) => r._measurement == \"dailyConsumption\")\n"
+						  + "|> sum()\n"
+						  + "t11 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartYear + ")\n"   
+						  + "|> filter(fn: (r) => r._measurement == \"dailySell\")\n"
+						  + "|> sum()\n"
+						  + "t12 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + StartYear + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"dailyBuy\")\n"
+						  + "|> sum()\n"
+						  + "t13 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + SumYear + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"dailyProduction\")\n"
+						  + "|> sum()\n"
+						  + "t14 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + SumYear + ")\n"   
+						  + "|> filter(fn: (r) => r._measurement == \"dailyConsumption\")\n"
+						  + "|> sum()\n"
+						  + "t15 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + SumYear + ")\n"   
+						  + "|> filter(fn: (r) => r._measurement == \"dailySell\")\n"
+						  + "|> sum()\n"
+						  + "t16 = from(bucket: \"auswertung/autogen\")\n"
+						  + "|> range(start: " + SumYear + ")\n"  
+						  + "|> filter(fn: (r) => r._measurement == \"dailyBuy\")\n"
+						  + "|> sum()\n"
+						  + "union(tables: [t1, t2, t3, t4, t5, t6, t7, t8, t9, t10, t11, t12, t13, t14, t15, t16])";	
 				
-				yearlyBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: " + StartYear + ")\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyBuy\" and r._field == \"abs\")\n"
-						+ "  |> sum()";	
-				
-				sumBody = "from(bucket: \"auswertung/autogen\")\n"
-						+ "  |> range(start: 2000-01-01)\n"
-						+ "  |> filter(fn: (r) => r._measurement == \"dailyBuy\" and r._field == \"abs\")\n"
-						+ "  |> sum()";	
-				
-				JsonObject dailyBuy = this.sendPostRequest(this.url, dailyBody);
-				JsonObject monthlyBuy = this.sendPostRequest(this.url, weeklyBody);
-				JsonObject yearlyBuy = this.sendPostRequest(this.url, yearlyBody);
-				JsonObject sumBuy = this.sendPostRequest(this.url, sumBody);
-				
-				int idailyBuy = dailyBuy.getAsJsonObject().get("_value").getAsInt();
-				this.channel(Summary.ChannelId.DAILY_BUY).setNextValue(idailyBuy);
-				this.channel(Summary.ChannelId.MONTHLY_BUY).setNextValue(monthlyBuy.getAsJsonObject().get("_value").getAsInt() + idailyBuy);
-				this.channel(Summary.ChannelId.YEARLY_BUY).setNextValue(yearlyBuy.getAsJsonObject().get("_value").getAsInt() + idailyBuy);
-				this.channel(Summary.ChannelId.SUM_BUY).setNextValue(sumBuy.getAsJsonObject().get("_value").getAsInt() + idailyBuy);
+				int dailyProduction = 0;
+				int dailyConsumption = 0;
+				int dailySell = 0;
+				int dailyBuy = 0;	
+				String[] arr = this.sendPostRequest(this.url, Body);
+				if (arr != null && arr.length > 4) {
+					String[] col = arr[3].split(",");
+					int indexStart = Arrays.asList(col).indexOf("_start");
+					int indexField = Arrays.asList(col).indexOf("_field");
+					int indexMeasurement = Arrays.asList(col).indexOf("_measurement");
+					int indexValue = Arrays.asList(col).indexOf("_value");
+									
+					// First find daily values
+					for (int i=4; i < arr.length; i++) {
+						String[] values = arr[i].split(",");
+						if (values[indexField].equals("_sum/ProductionActivePower") && values[indexStart].startsWith(Today)) {
+							dailyProduction = (int)Double.parseDouble(values[indexValue]);	
+							this.channel(Summary.ChannelId.DAILY_PRODUCTION).setNextValue(dailyProduction);
+						}
+						else if (values[indexField].equals("_sum/ConsumptionActivePower") && values[indexStart].startsWith(Today)) {
+							dailyConsumption = (int)Double.parseDouble(values[indexValue]);		
+							this.channel(Summary.ChannelId.DAILY_CONSUMPTION).setNextValue(dailyConsumption);
+						}
+						else if (values[indexField].equals("_sum/GridActivePower") && Double.parseDouble(values[indexValue]) < 0 && values[indexStart].startsWith(Today)) {
+							dailySell = Math.abs((int)Double.parseDouble(values[indexValue]));	
+							this.channel(Summary.ChannelId.DAILY_SELL).setNextValue(dailySell);
+						}
+						else if (values[indexField].equals("_sum/GridActivePower") && Double.parseDouble(values[indexValue]) > 0 && values[indexStart].startsWith(Today)) {
+							dailyBuy = (int)Double.parseDouble(values[indexValue]);
+							this.channel(Summary.ChannelId.DAILY_BUY).setNextValue(dailyBuy);
+						}
+					}
+					
+					// Find other values
+					for (int i=4; i < arr.length; i++) {
+						String[] values = arr[i].split(",");
+						if (values[indexMeasurement].equals("dailyProduction")) {
+							if (values[indexStart].startsWith(StartMonth)) {
+								int Production = (int)Double.parseDouble(values[indexValue]) + dailyProduction;	
+								this.channel(Summary.ChannelId.MONTHLY_PRODUCTION).setNextValue(Production);
+							}		
+							else if (values[indexStart].startsWith(StartYear)) {
+								int Production = (int)Double.parseDouble(values[indexValue]) + dailyProduction;	
+								this.channel(Summary.ChannelId.YEARLY_PRODUCTION).setNextValue(Production);
+							}
+							else if (values[indexStart].startsWith(SumYear)) {
+								int Production = (int)Double.parseDouble(values[indexValue]) + dailyProduction;	
+								this.channel(Summary.ChannelId.SUM_PRODUCTION).setNextValue(Production);
+							}
+						}
+						else if (values[indexMeasurement].equals("dailyConsumption")) {
+							if (values[indexStart].startsWith(StartMonth)) {
+								int Consumption = (int)Double.parseDouble(values[indexValue]) + dailyConsumption;	
+								this.channel(Summary.ChannelId.MONTHLY_CONSUMPTION).setNextValue(Consumption);
+							}		
+							else if (values[indexStart].startsWith(StartYear)) {
+								int Consumption = (int)Double.parseDouble(values[indexValue]) + dailyConsumption;	
+								this.channel(Summary.ChannelId.YEARLY_CONSUMPTION).setNextValue(Consumption);
+							}
+							else if (values[indexStart].startsWith(SumYear)) {
+								int Consumption = (int)Double.parseDouble(values[indexValue]) + dailyConsumption;	
+								this.channel(Summary.ChannelId.SUM_CONSUMPTION).setNextValue(Consumption);
+							}
+						}
+						else if (values[indexMeasurement].equals("dailySell")) {
+							if (values[indexStart].startsWith(StartMonth)) {
+								int Sell = Math.abs((int)Double.parseDouble(values[indexValue])) + dailySell;	
+								this.channel(Summary.ChannelId.MONTHLY_SELL).setNextValue(Sell);
+							}		
+							else if (values[indexStart].startsWith(StartYear)) {
+								int Sell = Math.abs((int)Double.parseDouble(values[indexValue])) + dailySell;	
+								this.channel(Summary.ChannelId.YEARLY_SELL).setNextValue(Sell);
+							}
+							else if (values[indexStart].startsWith(SumYear)) {
+								int Sell = Math.abs((int)Double.parseDouble(values[indexValue])) + dailySell;	
+								this.channel(Summary.ChannelId.SUM_SELL).setNextValue(Sell);
+							}
+						}
+						else if (values[indexMeasurement].equals("dailyBuy")) {
+							if (values[indexStart].startsWith(StartMonth)) {
+								int Buy = (int)Double.parseDouble(values[indexValue]) + dailyBuy;	
+								this.channel(Summary.ChannelId.MONTHLY_BUY).setNextValue(Buy);
+							}		
+							else if (values[indexStart].startsWith(StartYear)) {
+								int Buy = (int)Double.parseDouble(values[indexValue]) + dailyBuy;	
+								this.channel(Summary.ChannelId.YEARLY_BUY).setNextValue(Buy);
+							}
+							else if (values[indexStart].startsWith(SumYear)) {
+								int Buy = (int)Double.parseDouble(values[indexValue]) + dailyBuy;	
+								this.channel(Summary.ChannelId.SUM_BUY).setNextValue(Buy);
+							}
+						}
+					}
+				}
 				
 				this.cycle = 1;
 			}
@@ -213,7 +255,7 @@ public class SummaryImpl extends AbstractOpenemsComponent implements Summary, Op
 		}
 		catch (OpenemsNamedException e)
 		{
-			
+			this.logError(this.log, e.getMessage());
 		}
 	}
 	
@@ -221,10 +263,10 @@ public class SummaryImpl extends AbstractOpenemsComponent implements Summary, Op
 	 * Sends a POST request to the Influxdb API.
 	 * 
 	 * @param endpoint the REST Api endpoint
-	 * @return a JsonObject
+	 * @return a StringArray
 	 * @throws OpenemsNamedException on error
 	 */
-	private JsonObject sendPostRequest(String URL, String query) throws OpenemsNamedException {
+	private String[] sendPostRequest(String URL, String query) throws OpenemsNamedException {
 		try {
 			URL url = new URL(URL);
 			HttpURLConnection con = (HttpURLConnection) url.openConnection();
@@ -234,8 +276,8 @@ public class SummaryImpl extends AbstractOpenemsComponent implements Summary, Op
 			con.addRequestProperty("Content-Type", "application/vnd.flux");
 			con.setRequestProperty("Content-Length", Integer.toString(query.length()));			
 			con.getOutputStream().write(query.getBytes("UTF8"));
-			con.setConnectTimeout(5000);
-			con.setReadTimeout(5000);
+			con.setConnectTimeout(1000);
+			con.setReadTimeout(1000);
 			int status = con.getResponseCode();
 			String body;
 			try (BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()))) {
@@ -249,17 +291,8 @@ public class SummaryImpl extends AbstractOpenemsComponent implements Summary, Op
 				body = content.toString();
 			}
 			if (status < 300) {
-				// Parse csv-response to JSON
-				JsonObject json = new JsonObject();	
-				String[] arr = body.split("\n");
-				if (arr.length > 4) {
-					String[] col = arr[3].split(",");		
-					for (int i=4; i<arr.length; i++) {
-						String[] val = arr[i].split(",");			
-						json.addProperty(col[col.length-1], Double.valueOf(val[val.length-1]));
-					}				
-				}
-				return json;
+				String[] arr = body.split("\n");				
+				return arr;
 			} else {
 				throw new OpenemsException(
 						"Error while reading from InfluxDB API. Response code: " + status + ". " + body);
